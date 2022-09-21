d4cfd6338b5f1dd7387d5fb2155337c587870212
enriqrafa <enriquerafalogo@gmail.com>
2022-09-20 21:30:28 -0300

    new file:   Clase 1/codigo.js 	new file:   Clase 1/index.html 	new file:   Clase 2/condicionales.js 	new file:   Clase 2/index.html

---
+++ Clase 1/codigo.js
@@ -0,0 +1,10 @@
+let nombreIngresado = prompt("Ingrese su nombre");
+alert(nombreIngresado)
+let año = prompt("Ingrese su año de nacimiento")
+let Edad = 2022 - año
+alert (Edad)
+
+let Precio = parseFloat(prompt("Ingrese el precio neto"));
+let PriceafVAT = Precio*1.19
+alert(PriceafVAT)
+
---
+++ Clase 2/condicionales.js
@@ -0,0 +1,57 @@
+/* //if
+let edad=18
+
+if (edad==18)
+{
+    console.log("Ya eres mayor de edad")
+}
+
+
+let user = prompt("Ingrese Usuario")
+
+if (user=="Enriqrafa")
+{
+    console.log("Bienvenido Enrique")
+
+}else{
+    console.log("Usuario no registrado")
+} */
+
+//If...else...if...else
+/* let codProducto = prompt("Ingrese el código del producto y le diré su precio")
+
+if(codProducto == "1212")
+{
+    console.log("Mouse marca Genius - Modelo DX100 - $ 999.99")
+}else if(codProducto=="2222"){
+    console.log("Mouse marca Logitech - Modelo CX245 - $ 805.99")
+}else if(codProducto=="1133"){
+    console.log("Mouse Pad Gamer - $6000")
+}else{
+    console.log("Producto sin stock")
+} */
+
+/* const Año=2022
+let licConducir = parseInt(prompt("Ingrese el año de vencimiento de su licencia de conducir"))
+
+if(licConducir<=Año)
+{
+    console.log("Su licencia está vencida o por vencer")
+}
+else{
+    console.log("Su licencia está vigente")
+}
+
+ */
+
+// == Compara por valor
+let num1=1
+let numUno="1"
+let sonIguales = (num1==numUno)
+console.log(sonIguales)
+
+// === Compara por tipo y valor
+
+let sonEstrictamenteIguales = (num1 ===numUno)
+console.log(sonEstrictamenteIguales)
+
