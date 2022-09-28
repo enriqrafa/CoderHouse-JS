e1dc1863f7e5af8c7b9d5794fb8f24e256188888
enriqrafa <enriquerafalogo@gmail.com>
2022-09-27 21:13:17 -0300

    new file:   Clase 4/index.html 	new file:   Clase 4/script.js

---
+++ Clase 4/script.js
@@ -0,0 +1,75 @@
+/* function armarFrase(palabra1,palabra2){
+    console.log("La frase es: "+palabra1+" "+palabra2+".")
+}
+
+armarFrase("Tengo","hambre")
+armarFrase("Que","Frío")
+
+
+function nueva(){
+    console.log("Nueva funcion")
+}
+
+nueva()
+
+let p1=prompt("Ingresa la primera palabra")
+let p2=prompt("Ingresa la segunda palabra")
+
+armarFrase(p1,p2) */
+
+
+//Declaración de variable para guardar el resultado de la suma
+/* let resultado = 0;
+//Función que suma dos números y asigna a resultado
+function sumar(primerNumero, segundoNumero) {
+    resultado = primerNumero + segundoNumero
+}
+//Función que muestra resultado por consola
+function mostrar(mensaje) {
+    console.log(mensaje)
+}
+//Llamamos primero a sumar y luego a mostrar
+sumar(6, 3);            
+mostrar(resultado);  */
+
+
+//funcion con parametros y return
+/* let precioProd=parseFloat(prompt("Ingresa el precio del producto"));
+
+function calcularIva(precio){
+    return precio * 0.21;
+}
+
+//precio=100 -> iva=21
+
+let iva = calcularIva(precioProd);
+console.log("El valor del IVA es $"+iva);
+
+function mostrarPrecioFinal(precioIngresado,ivaCalculado){
+    console.log("El precio con iva incluido es $"+ (precioIngresado+ivaCalculado));
+}
+
+mostrarPrecioFinal(precioProd,iva); */
+
+/* function calculadora(primerNumero, segundoNumero, operacion) {
+    switch (operacion) {
+        case "+":
+            return primerNumero + segundoNumero;
+            break;
+        case "-":
+            return primerNumero - segundoNumero;
+            break;
+        case "*":
+            return primerNumero * segundoNumero;
+            break;
+        case "/":
+            return primerNumero / segundoNumero;
+            break;
+        default:
+            return 0;
+            break;
+    }
+}
+console.log(calculadora(10, 5, "*"));
+ */
+
